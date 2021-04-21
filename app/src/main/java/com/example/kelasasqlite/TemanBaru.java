fef93668776146a1356fdc6b589bd941bbaba622
PinkyRhinos <muhammadriskidhiaulhaq112@gmail.com>
2021-04-22 00:31:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kelasasqlite/TemanBaru.java
@@ -0,0 +1,34 @@
+package com.example.kelasasqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class TemanBaru extends AppCompatActivity {
+    private TextInputEditText tNama,tTelpon;
+    private Button simpanBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman_baru);
+
+        tNama = (TextInputEditText)findViewById(R.id.tietNama);
+        tTelpon = (TextInputEditText)findViewById(R.id.tietTelpon);
+        simpanBtn = (Button)findViewById(R.id.buttonSave);
+
+        simpanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tNama.getText().toString().equals("")||tTelpon.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"Data belum komplit !",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
